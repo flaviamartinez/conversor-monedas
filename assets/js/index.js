50957672ac45bcc8a9a571889b8cddb4710fed1c
Flavia Martinez <flavia.martinez@uc.cl>
2025-01-09 22:01:54 -0300

    Add negative input value validation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,129 +1,139 @@
 const apiURL = "https://mindicador.cl/api/";
-const currenciesArray = []
+const currenciesArray = [];
 
 async function getCurrencies() {
-	try {
-		const res = await fetch(apiURL);
-		const currencies = await res.json();
-		return currencies
-	} catch (e) {
-		alert(e.message)
-	}
+  try {
+    const res = await fetch(apiURL);
+    const currencies = await res.json();
+    return currencies;
+  } catch (e) {
+    alert(e.message);
+  }
 }
 
 async function storeCurrencies() {
-	try {
-		const currencies = await getCurrencies();
-		
-		const { version, autor, fecha, ...monedas } = currencies;
-	
-		Object.values(monedas).forEach(moneda => {
-			currenciesArray.push(moneda)
-		});
-	} catch (e) {
-		alert(e.message)
-	}
+  try {
+    const currencies = await getCurrencies();
+
+    const { version, autor, fecha, ...monedas } = currencies;
+
+    Object.values(monedas).forEach((moneda) => {
+      currenciesArray.push(moneda);
+    });
+  } catch (e) {
+    alert(e.message);
+  }
 }
 
-async function getHistoricalData(currency){
-	apiHistoryURL = `${apiURL}${currency}`;
+async function getHistoricalData(currency) {
+  const apiHistoryURL = `${apiURL}${currency}`;
 
-	try{
-		const res = await fetch(apiHistoryURL);
-		const history = await res.json();
-		return history
-	} catch (e) {
-		alert(e.message)
-	}
+  try {
+    const res = await fetch(apiHistoryURL);
+    const history = await res.json();
+    return history;
+  } catch (e) {
+    alert(e.message);
+  }
 }
 
-async function storeHistory(currency){
-	try{
-		const history = await getHistoricalData(currency);
-		const serie = history.serie;
-		return serie
-	} catch (e) {
-		alert(e.message)
-	}
+async function storeHistory(currency) {
+  try {
+    const history = await getHistoricalData(currency);
+    const serie = history.serie;
+    return serie;
+  } catch (e) {
+    alert(e.message);
+  }
 }
 
-function createChartData(serie){
-	template = `<h1>Grafico</h1>
+function createChartData(serie) {
+  const template = `
+	<h2>Grafico de Evolución</h2>
 	<div id="grafico">
 		<canvas id="myChart" width="800" height="400"></canvas>
-	</div>`;
+	</div>
+	`;
 
-	serie.splice(10);
+  const filteredSerie = serie.slice(0, 10);
 
-	serie.sort((a, b) => new Date(a.fecha) - new Date(b.fecha));
+  filteredSerie.sort((a, b) => new Date(a.fecha) - new Date(b.fecha));
 
-	console.log(serie)
-	chart.innerHTML = template;
+  chart.innerHTML = template;
 
-	const labels = serie.map(obs => {
-		return obs.fecha.split("T")[0]
-	});
+  const labels = filteredSerie.map((obs) => {
+    return obs.fecha.split("T")[0];
+  });
 
-	const data = serie.map(obs => {
-		return obs.valor
-	});
+  const data = filteredSerie.map((obs) => {
+    return obs.valor;
+  });
 
-	const datasets = [
-		{
-			label: "Tipo de cambio",
-			borderColor: "rgb(255, 99, 132)",
-			data
-		}
-	];
+  const datasets = [
+    {
+      label: "Tipo de cambio",
+      borderColor: "rgb(255, 99, 132)",
+      data,
+    },
+  ];
 
-	return { labels, datasets }
+  return { labels, datasets };
 }
 
 function renderChart(serie) {
-	const data = createChartData(serie);
-	const config =  {
-		type: "line",
-		data,
-		options: {
-			plugins: {
-				legend: {
-					labels: {
-						usePointStyle: true,
-        				pointStyle: 'circle'
-					}
-				}
-			}
-		}
-	};
+  const data = createChartData(serie);
+  const config = {
+    type: "line",
+    data,
+    options: {
+      plugins: {
+        legend: {
+          labels: {
+            usePointStyle: true,
+            pointStyle: "circle",
+          },
+        },
+      },
+    },
+  };
 
-	const myChart = document.getElementById("myChart");
-	myChart.style.backgroundColor = "white";
-	new Chart(myChart, config);
+  const myChart = document.getElementById("myChart");
+  myChart.style.backgroundColor = "white";
+  new Chart(myChart, config);
 }
 
-storeCurrencies()
+const input = document.querySelector("input");
+const selectCurrency = document.getElementById("currencyDropdown");
+const btn = document.querySelector("button");
+const result = document.getElementById("result");
+const chart = document.getElementById("chart-container");
 
-const input = document.querySelector('input');
-const selectCurrency = document.getElementById('currencyDropdown');
-const btn = document.querySelector('button');
-const result = document.getElementById('result');
-const chart = document.getElementById('chart-container');
+storeCurrencies();
 
-btn.addEventListener('click', async () => {
-	amount = input.value;
-	currency = selectCurrency.value;
-	selectedCurrency = currenciesArray.find(c => c.codigo === currency);
+btn.addEventListener("click", async () => {
+  const amount = input.value;
+  const currency = selectCurrency.value;
+  const selectedCurrency = currenciesArray.find((c) => c.codigo === currency);
 
-	if (!selectedCurrency) {
-		alert("Debes seleccionar una moneda");
-		return;
-	  };
-	
-	currencyValue = selectedCurrency.valor;
+  if (amount === "") {
+    alert("Debes ingresar monto en CLP");
+    return;
+  }
 
-	result.innerHTML = `Resultado: ${(amount / currencyValue).toFixed(2)}`;
+  if (amount <= 0) {
+    alert("Debes ingresar un valor mayor a 0");
+    return;
+  }
 
-	const serie = await storeHistory(currency);
-	renderChart(serie)
-}
-)
+  if (!selectedCurrency) {
+    alert("Debes seleccionar una moneda");
+    return;
+  }
+
+  const currencyValue = selectedCurrency.valor;
+
+  result.innerHTML = `Resultado: ${(amount / currencyValue).toFixed(2)}`;
+
+  const serie = await storeHistory(currency);
+  renderChart(serie);
+});
